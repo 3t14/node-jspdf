c8701398dd01b0cf8588efede250b19f58be9b54
Michiru Tanaka <michiru@r4hs.com>
2016-09-23 11:48:44 +0900

    modified to jsPDF_ja

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ if [ ! -d vendor/jsPDF ]; then
     wget --no-check-certificate -O jsPDF.zip https://github.com/3t14/jsPDF_ja/archive/e868076e34597bba0370ca615435b1e2dc042410.zip
     unzip jsPDF.zip
     mkdir vendor
-    mv jsPDF-* vendor/jsPDF
+    mv jsPDF_ja-* vendor/jsPDF
     rm jsPDF.zip
 fi
